0457633e7231499c89b542355f672d1b0e0d0b07
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-04-02 13:22:41 -0300

    Update MovieContext.js

--- projeto2/src/context/MovieContext.js
+++ projeto2/src/context/MovieContext.js
@@ -1,10 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 
 function MovieContext() {
 
     // const MovieContext = createContext();
+
+    const [videos, setVideos] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    function runSearch(query){
+
+        setLoading(true);
+
+        try{
+            
+        }catch(error){
+            console.error("Erro ao buscar vídeos: ", error);
+        }finally{
+            setLoading(false);
+        }
+    }
 
     return (
         <>
