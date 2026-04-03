f5d2ebd2bea104ebdc351956edbe69c3a0c4e1fb
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-04-03 16:39:27 -0300

    Realizando a conexão com a API

--- projeto2/src/components/Gallery.js
+++ projeto2/src/components/Gallery.js
@@ -1,10 +1,45 @@
+import Movie from './Movie.js';
 
+function Gallery({ data }) {
 
-function Gallery({data}){
+    const results = data;
 
-    return(
-        <div>
-            
+    return (
+        <div className="photo-container">
+            {
+                results.length > 0 ?
+                    (
+                        <ul>
+                            {results.map((movie) => {
+                                // const { adult, backdrop_path, genre_ids, id, original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count } = movie;
+
+                                const { id, overview, poster_path, release_date, title, video, vote_average, vote_count } = movie;
+
+                                // const { id, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count } = movie;
+
+                                const url = poster_path
+                                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                                    : `https://via.placeholder.com/500x750?text=Sem+Foto`;
+                                // : `https://via.placeholder.com/500x750?text=No+Image`;
+
+                                return (
+                                    <Movie
+                                        key={id}
+                                        title={title}
+                                        url={url}
+                                        overview={overview}
+                                        rating={vote_average}
+                                        votes={vote_count}
+                                        date={release_date}
+                                        video={video}
+                                    />
+                                );
+                            })}
+                        </ul>
+                    )
+                    :
+                    (<h2>No Movies Found</h2>)
+            }
         </div>
     );
 }
---
+++ projeto2/src/components/Movie.js
@@ -0,0 +1,31 @@
+
+function Movie({ title, url, overview, rating, votes, date, video }) {
+
+    return (
+
+        //    <li>
+        //         <img src={url} alt={title}></img>
+        //    </li>
+
+        <li className="movie-card">
+            {/* Foto/Poster */}
+            <img src={url} alt={title} />
+
+            <div className="movie-info">
+                {/* Título */}
+                <h3>{title}</h3>
+
+                {/* Avaliação e Contagem de Votos */}
+                <div className="rating-box">
+                    <span>⭐ {rating}</span>
+                    <small>({votes} votos)</small>
+                </div>
+
+                {/* Overview (Sinopse) */}
+                <p className="overview">{overview}</p>
+            </div>
+        </li>
+    );
+}
+
+export default Movie;
